9945215f0cc2276bc76053db85f0c95059f93bbd
KrishnaSSH <krishna.pytech@gmail.com>
2026-05-30 22:59:59 +0530

    test httpchecker

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
 
-// TODO
+	"github.com/krishnassh/picostatus/internal/checker"
+)
+
+func main() {
+	fmt.Print("url: ")
+
+	url, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	url = strings.TrimSpace(url)
+
+	res := checker.HTTPChecker{URL: url}.Run(context.Background())
+
+	fmt.Println(res)
+}
